09d45e6e1aa0713e11c01e2614912b590d1d5fb5
Facundo Guini <facundo@chazki.com>
2017-09-18 22:15:39 -0300

    entities

---
+++ LeatherGoods/Shared/Entities/Country.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+
+
+namespace Entities
+{
+    /// <summary>
+    /// Represents a row of entity data.
+    /// </summary>
+    [Serializable]
+    [DataContract]
+    public partial class Country : EntityBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        [DisplayName("Id")]
+        [Browsable(false)]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        [DisplayName("Created On")]
+        public DateTime CreatedOn { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        [DisplayName("Created By")]
+        public int? CreatedBy { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        [DisplayName("Changed On")]
+        public DateTime ChangedOn { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        [DisplayName("Changed By")]
+        public int? ChangedBy { get; set; }
+
+        
+    }
+}
