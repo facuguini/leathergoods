90bf4a9766fc64afa14a31eba2041591030e6968
Marco Puccio <mpuccio90@gmail.com>
2017-10-24 00:42:14 +0000

    Meta seo class utils

--- Shared/Framework/Seo/Meta.cs
+++ Shared/Framework/Seo/Meta.cs
@@ -1,8 +1,20 @@
 using System;
+using Framework.Extensions;
 
 namespace Framework.Seo
 {
     public class Meta
     {
+        public static string Title(string title)
+        {
+            return $"<title>{title}</title>";
+
+        }
+
+        public static string Description(string description)
+        {
+            string safeDescription = description.StripTags().Truncate(160);
+            return $"<meta name=\"description\" content=\"{safeDescription}\">";
+        }
     }
 }
